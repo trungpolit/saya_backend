9fad7198d847b25ec4ab0c081fcf699601297e84
trungpolit <ngotrung.poli.t@gmail.com>
2016-10-10 00:20:01 +0700

    Chinh sua lai sh cho crontab

--- app/Crontab/CrontabDailyProductReports_index.sh
+++ app/Crontab/CrontabDailyProductReports_index.sh
@@ -11,4 +11,4 @@ FILE_NAME="${FILE_NAME%.*}"
    rm -f $PID_FILE
 }
 echo $$ > $PID_FILE
-php $APP_PATH/cron_dispatcher.php /CrontabOrdersDistributors/send >> $APP_PATH/tmp/logs/$FILE_NAME.out.log 2>&1
+php $APP_PATH/cron_dispatcher.php /CrontabDailyProductReports/index >> $APP_PATH/tmp/logs/$FILE_NAME.out.log 2>&1
